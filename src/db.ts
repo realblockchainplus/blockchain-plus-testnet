aa3e993a1afab031a8cbafe34824a72c6f719026
Scott Donnelly <scott.donnelly790@gmail.com>
2018-06-18 09:41:09 -0400

    no message

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,13 @@
+import * as minimist from 'minimist';
 import * as mongoose from 'mongoose';
 
+const argv = minimist(process.argv.slice(2));
 const conn = mongoose.createConnection(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP}/${process.env.DB_NAME}`);
+
+conn.on('open', () => {
+  if (argv.ci === true) {
+    process.exit();
+  }
+});
 
 export { conn };
