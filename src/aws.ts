1d83ece65eba1cc2118ef879e0b3d209c36330cd
Scott <scott.donnelly790@gmail.com>
2018-06-30 14:34:40 -0400

    no message

--- src/aws.ts
+++ src/aws.ts
@@ -37,47 +37,69 @@ const initEC2 = (region?: AWSRegionCode): AWS.EC2 => {
   return ec2;
 };
 
-const createEC2Instance = (type: PodType, region: AWSRegionCode, nodeCount: number, imageName: string) => {
+const createEC2Instance = (type: PodType, region: AWSRegionCode, nodeCount: number, imageName: string,
+  callback: (instance: AWS.EC2.Instance) => void = () => {}) => {
+  console.log('[createEC2Instance]');
   const ec2 = initEC2(region);
-  const InstanceType = type === PodType.REGULAR_POD ? 't2.regular' : 't2.large';
+  const InstanceType = type === PodType.REGULAR_POD ? 't2.medium' : 't2.large';
   const tag = type === PodType.REGULAR_POD ? regularTag : partnerTag;
 
   const IamInstanceProfile: AWS.EC2.IamInstanceProfile = {
     Arn: 'arn:aws:iam::490668483643:instance-profile/EC2-bcp-tn-regular',
   };
-  getImageId(ec2, imageName, (ImageId) => {
-    const params: AWS.EC2.RunInstancesRequest = {
-      IamInstanceProfile,
-      ImageId,
-      InstanceType,
-      KeyName: 'bcp-tn-node',
-      MinCount: nodeCount,
-      MaxCount: nodeCount,
-    };
 
-    ec2.runInstances(params, (err, data) => {
-      if (err) {
-        console.log(`[runInstances] Error: ${err}`);
-      }
-      else {
-        for (let i = 0; i < data.Instances!.length; i += 1) {
-          const instance = data.Instances![i];
-          const tags = {
-            Resources: [instance.InstanceId!],
-            Tags: [tag],
-          };
-          ec2.createTags(tags, (_err, _data) => {
-            if (_err) {
-              console.log(`[createTags] Error: ${_err}`);
-            }
-            else {
-              console.log('Instance tagged');
-              console.log(data);
-            }
-          });
+  ec2.describeSecurityGroups((e, d) => {
+    if (e) {
+      console.log(`[describeSecurityGroups] Error: ${e}`);
+    }
+    else {
+      // console.dir(`[describeSecurityGroups] Success: ${JSON.stringify(d)}`);
+      let securityGroup: AWS.EC2.SecurityGroup;
+      for (let i = 0; i < d.SecurityGroups!.length; i += 1) {
+        const sg = d.SecurityGroups![i];
+        if (sg.GroupName === 'bcp-tn-node-default') {
+          securityGroup = sg;
         }
       }
-    });
+      getImageIdByImageName(ec2, imageName, (ImageId) => {
+        const params: AWS.EC2.RunInstancesRequest = {
+          IamInstanceProfile,
+          ImageId,
+          InstanceType,
+          KeyName: 'bcp-tn-node',
+          MinCount: nodeCount,
+          MaxCount: nodeCount,
+          SecurityGroupIds: [securityGroup.GroupId!],
+          UserData: Buffer.from('#!/bin/bash;cd blockchain-plus-testnet;sudo git pull origin master;sudo npm run compile;sudo npm run start-regular').toString('base64'),
+        };
+
+        ec2.runInstances(params, (err, data) => {
+          if (err) {
+            console.log(`[runInstances] Error: ${err}`);
+          }
+          else {
+            for (let i = 0; i < data.Instances!.length; i += 1) {
+              const instance = data.Instances![i];
+              // console.dir(`[runInstances] Success: ${JSON.stringify(instance)}`);
+              const tags = {
+                Resources: [instance.InstanceId!],
+                Tags: [tag],
+              };
+              ec2.createTags(tags, (_err, _data) => {
+                if (_err) {
+                  console.log(`[createTags] Error: ${_err}`);
+                }
+                else {
+                  console.log('Instance tagged');
+                  console.log(data);
+                  callback(instance);
+                }
+              });
+            }
+          }
+        });
+      });
+    }
   });
 };
 
@@ -124,17 +146,51 @@ const createEC2Cluster = (totalNodes: number, regions: AWSRegionCode[], imageNam
   }));
 };
 
-const getImageId = (ec2: AWS.EC2, imageName: string, callback: (imageId: string | undefined) => void) => {
-  ec2.describeImages((err, data) => {
+const getImagesByTag = (ec2: AWS.EC2, imageTag: AWS.EC2.Tag, callback: (images: AWS.EC2.Image[] | undefined) => void) => {
+  const params: AWS.EC2.DescribeImagesRequest = {
+    Filters: [
+      {
+        Name: imageTag.Key as string,
+        Values: [
+          imageTag.Value as string,
+        ],
+      },
+    ],
+  };
+
+  ec2.describeImages(params, (err, data) => {
+    if (err) {
+      console.log(`[describeImages] Error: ${err}`);
+    }
+    else {
+      const images = data.Images;
+      callback(images);
+    }
+  });
+}
+
+const getImageIdByImageName = (ec2: AWS.EC2, imageName: string, callback: (imageId: string | undefined) => void) => {
+  const params: AWS.EC2.DescribeImagesRequest = {
+    Filters: [
+      {
+        Name: 'name',
+        Values: [
+          imageName,
+        ],
+      },
+    ],
+  };
+  ec2.describeImages(params, (err, data) => {
+    if (data.Images!.length === 0) {
+      console.log(`${imageName} not found!`);
+    }
     if (err) {
       console.log(err);
     }
-    const images = data.Images;
-    if (images) {
-      for (let i = 0; i < images.length; i += 1) {
-        const image = images[i];
-        image.Name === imageName ? callback(image.ImageId) : null;
-      }
+    else {
+    // console.log(`[describeImages] Images: ${JSON.stringify(data)}`);
+      const images = data.Images;
+      callback(images![0].ImageId);
     }
   });
 };
@@ -165,9 +221,14 @@ const configureSecurityGroups = (create: boolean) => {
               else {
                 const inboundParams: AWS.EC2.AuthorizeSecurityGroupIngressRequest = {
                   GroupName: 'bcp-tn-node-default',
-                  IpProtocol: '-1',
-                  ToPort: -1,
-                  FromPort: -1,
+                  IpPermissions: [
+                    {
+                      IpProtocol: '-1',
+                      ToPort: -1,
+                      FromPort: -1,
+                      IpRanges: [{ CidrIp: '0.0.0.0/0' }],
+                    },
+                  ],
                 };
                 ec2.authorizeSecurityGroupIngress(inboundParams, (__err, __data) => {
                   if (__err) {
@@ -207,4 +268,37 @@ const configureSecurityGroups = (create: boolean) => {
   }
 };
 
-export { AWSRegionCode, createEC2Instance, createEC2Cluster, configureSecurityGroups };
+const createNewImage = (region: AWSRegionCode, commitTag: string) => {
+  const ec2 = initEC2(region);
+  const imageTag: AWS.EC2.Tag = {
+    Key: 'tag:category',
+    Value: 'bcp-tn-node-image'
+  };
+  getImagesByTag(ec2, imageTag, (images) => {
+    images!.sort((a, b) => {
+      return Date.parse(a.CreationDate as string) - Date.parse(b.CreationDate as string);
+    });
+    const mostRecentImage = images![0];
+    createEC2Instance(PodType.REGULAR_POD, region, 1, mostRecentImage.Name!, (instance: AWS.EC2.Instance) => {
+      const waitForParams: AWS.EC2.DescribeInstancesRequest = {
+        InstanceIds: [instance.InstanceId!],
+      };
+      ec2.waitFor('instanceRunning', waitForParams, (e, d) => {
+        const params: AWS.EC2.CreateImageRequest = {
+          InstanceId: instance.InstanceId as string,
+          Name: commitTag,
+        };
+        ec2.createImage(params, (err, data) => {
+          if (err) {
+            console.log(`[createImage] Error: ${err}`);
+          }
+          else {
+            console.log(`[createImage] Success: ${JSON.stringify(data)}`);
+          }
+        });
+    });
+    });
+  });
+};
+
+export { AWSRegionCode, createEC2Instance, createEC2Cluster, createNewImage, configureSecurityGroups };
