52cdfd4fdbd90031031d71d789e63fc6c87b8b62
Scott Donnelly <scott.donnelly790@gmail.com>
2018-07-04 15:29:58 -0400

    create-image script

---
+++ scripts/create-image.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import { AWSRegionCode, createNewImage } from '../src/aws';
+
+const regions = Object.keys(AWSRegionCode).map((key: string) => AWSRegionCode[key as any]);
+
+for (let i = 0; i < regions.length; i += 1) {
+  const region = regions[i];
+  createNewImage(region as AWSRegionCode, process.env.TRAVIS_TAG as string);
+}
